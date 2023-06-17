6d6317fcb792a9af10470db60407f5516a22a454
Katya <Katya11091@outlook.com>
2023-06-17 12:00:07 +0300

    f-string

--- stepic_ex/sandbox.py
+++ stepic_ex/sandbox.py
@@ -1,19 +1,2 @@
-#var_1, var_2 = map(str, input().split())
-# string_01 = input()
-# string_02 = input()
-
-msg = "abrakadabra"
-print(msg.isalpha())
-
-d = "abc"
-print(d.ljust(10, "*"))
-
-print("Иванов Иван Иванович".split(" "))
-digs = "1, 2,3, 4,5,6"
-print(digs.replace(" ", '').split(","))
-', '.join(digs)
-print(digs)
-
-fio = "Иванов Иван Иванович"
-fio_2 = ','.join(fio.split())
-print(fio_2)
+rate = float(input())
+rs = int(input())
