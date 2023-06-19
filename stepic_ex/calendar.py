45e0cbbf2680b8765247dd2d7106850f4d9fcae8
Katya <Katya11091@outlook.com>
2023-06-19 08:53:52 +0300

    continuos

--- stepic_ex/calendar.py
+++ stepic_ex/calendar.py
@@ -1,30 +1,35 @@
-month, number = map(int, input().split())
+def get_prev_and_next_date():
+    month, number = map(int, input().split())
+    print(f"Текущий День: {number}.{month}")
+    previous_day = previous_month = next_day = next_month = 0
 
-previous_day = 0
-previous_month = 0
+    months_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if number != 1:
+        days_in_month = months_list[month - 1]
+        if number == days_in_month:
+            print("Последний день месяца")
+            next_day = 1
+            next_month = month + 1
+        else:
+            print("Любой день")
+            next_day = number + 1
+            next_month = month
+        previous_day = number - 1
+        previous_month = month
+    else:
+        print("1 число")
+        previous_day = months_list[month - 2]
+        previous_month = month - 1
+        next_day = number + 1
+        next_month = month
 
-next_day = 0
-next_month = 0
+    previous_day = str(previous_day).rjust(2, '0')
+    previous_month = str(previous_month).rjust(2, '0')
+    next_day = str(next_day).rjust(2, '0')
+    next_month = str(next_month).rjust(2, '0')
 
-months_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-if number == 1:
-    print("Pos 1")
-    previous_day = months_list[month - 2]
-    previous_month = month - 1
-    next_day = number + 1
-    next_month = month
-elif number in (28, 30, 31):
-    print("Pos 2")
-    previous_day = number - 1
-    previous_month = month
-    next_day = 1
-    next_month = month + 1
-else:
-    print("Pos 3")
-    previous_day = number - 1
-    previous_month = month
-    next_day = number + 1
-    next_month = month
+    print(f'{previous_day}.{previous_month} {next_day}.{next_month}')
 
-print(f"Предыдущий день: {previous_day}.{previous_month}")
-print(f"Следующий день: {next_day}.{next_month}")
+
+if __name__ == "__main__":
+    get_prev_and_next_date()
--- stepic_ex/sandbox.py
+++ stepic_ex/sandbox.py
@@ -1,44 +1,7 @@
-month, number = map(int, input().split())
+k = int(input())
 
-previous_day = 0
-next_day = 0
-previous_month = 0
-next_month = 0
+week_days = ('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье')
+name_of_day = week_days[k % 7 - 1]
+print(name_of_day)
 
-
-if number == 28 and month == 2:
-    next_day = 1
-    next_month += month
-
-
-
-if month in [1, 3, 5, 7, 8, 10, 12] :
-    if 2 <= number <= 30:
-        print("in month 31")
-        previous_day = number -1
-        next_day = number + 1
-    elif number == 1:
-        previous_day = 31
-
-
-elif month == 2 :
-    if 2 <= number <= 27:
-        print("in month 28")
-        previous_day = number - 1
-        next_day = number + 1
-
-else:
-
-    if 2 <= number <= 29:
-        print("in month 30")
-        previous_day = number - 1
-        next_day = number + 1
-
-
-
-previous_day = str(previous_day)
-next_day = str(next_day)
-month = str(month)
-next_month = str(next_month)
-print(f"{previous_day.rjust(2, '0')}.{month.rjust(2, '0')} {next_day.rjust(2, '0')}.{next_month.rjust(2, '0')}")
 
